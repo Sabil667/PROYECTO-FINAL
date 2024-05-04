18d9a8c22c6611669b8ddb7117eedc407646a5ee
Sabil667 <msabi@myuax.com>
2024-05-04 23:25:44 +0200

    .

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @org.junit.Test
+    public void main() {
+    }
+
+    @org.junit.Test
+    public void placeComponents() {
+    }
+
+    @org.junit.Test
+    public void mostrarOpcionesPostExperimento() {
+    }
+}
